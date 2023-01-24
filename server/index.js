8b14dff1eb62e5792e04d6040249e622d8e71c36
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-01-24 15:25:35 -0500

    deploy prove

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-// const { database } = require("./db");
+const { database } = require("./src/db");
 const router = require("./src/routes/index");
 const { routerRegistration } = require("./src/routes/authorization");
 const cors = require("cors");
@@ -16,15 +16,15 @@ app.get("/", (req, res) => {
   return res.json({message: "Hello, world from jub hunter api"});
 })
 
-// app.use("/registration", routerRegistration);
-// app.use("/", router);
+app.use("/registration", routerRegistration);
+app.use("/", router);
 
 try {
-  // database.sync({ force: false }).then(() => {
+  database.sync({ force: false }).then(() => {
     app.listen(port, async () => {
       console.log(`Server listening on port: ${port}`);
     });
-  // });
+  });
 } catch (error) {
   console.log(error);
 }
