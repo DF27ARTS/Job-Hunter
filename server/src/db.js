0716a2d6931e222b520c1808d84d6660a3cd00a7
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-01-23 20:42:57 -0500

    changing the web deply link

--- server/src/db.js
+++ server/src/db.js
@@ -6,23 +6,28 @@ const UserProfile = require("./models/user");
 const { Router } = require("express");
 const router = Router();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  logging: false,
-  native: false,
-  pool: {
-    max: 3,
-    min: 1,
-    idle: 10000,
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-    keepAlive: true,
-  },
-  ssl: true,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      logging: false,
+      native: false,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+  : new Sequelize(process.env.LOCAL_URL, {
+      logging: false,
+      native: false,
+    }); 
 
 Card(sequelize);
 UserProfile(sequelize);
--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 
+
 app.use("/registration", routerRegistration);
 app.use("/", router);
 
