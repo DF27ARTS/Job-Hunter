41063b05eff4fbed118b9b85f063fea0fc41e971
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-01-24 16:15:13 -0500

    serrver ready to deploy on render

--- server/src/db.js
+++ server/src/db.js
@@ -1,55 +1,55 @@
-// require("dotenv").config();
-// const { Sequelize, DataTypes } = require("sequelize");
-// const Card = require("./models/card");
-// const UserProfile = require("./models/user");
+require("dotenv").config();
+const { Sequelize, DataTypes } = require("sequelize");
+const Card = require("./models/card");
+const UserProfile = require("./models/user");
 
-// const { Router } = require("express");
-// const router = Router();
+const { Router } = require("express");
+const router = Router();
 
-// const sequelize = process.env.DATABASE_URL
-//   ? new Sequelize(process.env.DATABASE_URL, {
-//       logging: false,
-//       native: false,
-//       pool: {
-//         max: 3,
-//         min: 1,
-//         idle: 10000,
-//       },
-//       dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false,
-//         },
-//         keepAlive: true,
-//       },
-//       ssl: true,
-//     })
-//   : new Sequelize(process.env.LOCAL_URL, {
-//       logging: false,
-//       native: false,
-//     }); 
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      logging: false,
+      native: false,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+  : new Sequelize(process.env.LOCAL_URL, {
+      logging: false,
+      native: false,
+    }); 
 
-// Card(sequelize);
-// UserProfile(sequelize);
+Card(sequelize);
+UserProfile(sequelize);
 
-// try {
-//   sequelize.authenticate();
-//   console.log("Connection has been established successfully.");
-// } catch (error) {
-//   console.error("Unable to connect to the database:", error);
-// }
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
-// const { userProfile, card } = sequelize.models;
+const { userProfile, card } = sequelize.models;
 
-// userProfile.belongsToMany(card, {
-//   through: "user_card",
-// });
-// card.belongsToMany(userProfile, {
-//   through: "user_card",
-// });
+userProfile.belongsToMany(card, {
+  through: "user_card",
+});
+card.belongsToMany(userProfile, {
+  through: "user_card",
+});
 
-// module.exports = {
-//   database: sequelize,
-//   ...sequelize.models,
-//   router: router,
-// };
+module.exports = {
+  database: sequelize,
+  ...sequelize.models,
+  router: router,
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-// const { database } = require("./src/db");
-const router = require("./src/routes/index");
-const { routerRegistration } = require("./src/routes/authorization");
+const { database } = require("./db");
+const router = require("./routes/index");
+const { routerRegistration } = require("./routes/authorization");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
@@ -13,18 +13,18 @@ app.use(morgan("dev"));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.json({message: "Hello, world from jub hunter api"});
+  return res.send("<h1>Hello, world from jub hunter api</h1>")
 })
 
-// app.use("/registration", routerRegistration);
-// app.use("/", router);
+app.use("/registration", routerRegistration);
+app.use("/", router);
 
 try {
-  // database.sync({ force: false }).then(() => {
+  database.sync({ force: false }).then(() => {
     app.listen(port, async () => {
       console.log(`Server listening on port: ${port}`);
     });
-  // });
+  });
 } catch (error) {
   console.log(error);
 }
