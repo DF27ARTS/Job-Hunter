0704d531d38fcc4e163020e37867e9e152022f4f
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-02-01 16:25:22 -0500

    error fixed in the user

--- client/src/store/userSlice.ts
+++ client/src/store/userSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 import { getCards } from "./cardSlice"
 
-export const API_URL = import.meta.env.API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const saveToken = (token: string): void => {
   const response = localStorage.setItem("token", token)
